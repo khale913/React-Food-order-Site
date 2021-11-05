aa0c94f70eff6c408d6034c59b61c3d3dab3abe3
Kelton Hale <keltonhale33@gmail.com>
2021-11-05 10:05:25 -0600

    added modal for checkoutto show cart when selected

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import Cart from "./Components/Cart/Cart";
 import Header from "./Components/Layout/Header";
 import Meals from "./Components/Meals/Meals";
 
 function App() {
+  // STATE
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  // HANDLERS
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
+
   return (
     <Fragment>
-      <Cart />
-      <Header />
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
